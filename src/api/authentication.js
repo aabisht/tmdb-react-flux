80c46797b2c7173415577003da91b3a6094f8613
arunbisht <aabisht@gmail.com>
2021-05-18 16:46:16 +0530

    Authentication api added.

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -11,7 +11,52 @@ import { handleResponse, handleError, ApiParameters } from "./apiUtils";
 const baseURL = apiConstants.API_URL + "/authentication";
 
 // Create a temporary request token that can be used to validate a TMDB user login.
-export function getRequestToken(api_key) {
-  const url = baseURL + "/token/new?" + ApiParameters.setApiKey(api_key);
+export function getRequestToken() {
+  const url =
+    baseURL + "/token/new?" + ApiParameters.setApiKey(apiConstants.API_KEY);
   return fetch(url).then(handleResponse).catch(handleError);
+}
+
+// This method allows you to mark a movie or TV show as a favorite item.
+export function createSessionWithLogin(username, password, request_token) {
+  const url =
+    baseURL +
+    "/token/validate_with_login?" +
+    ApiParameters.setApiKey(apiConstants.API_KEY);
+
+  const mediaData = {
+    username: username,
+    password: password,
+    request_token: request_token,
+  };
+
+  return fetch(url, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ...mediaData,
+    }),
+  })
+    .then(handleResponse)
+    .catch(handleError);
+}
+
+// If you would like to delete (or "logout") from a session, call this method with a valid session ID.
+export function deleteSession(session_id) {
+  const url =
+    baseURL + "/session?" + ApiParameters.setApiKey(apiConstants.API_KEY);
+
+  const mediaData = {
+    session_id: session_id,
+  };
+
+  return fetch(url, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      ...mediaData,
+    }),
+  })
+    .then(handleResponse)
+    .catch(handleError);
 }
