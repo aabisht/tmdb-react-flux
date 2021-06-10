7f71bc4c705b2b3a672ca7c4d2ad75b9f8a7f27e
arunbisht <aabisht@gmail.com>
2021-06-10 20:52:00 +0530

    Logo and logo navigation styling added.

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logo.svg";
 
 function Header() {
@@ -7,13 +7,91 @@ function Header() {
     <div className="header-container">
       <div className="header-container-wrapper">
         <div className="container">
-          <div className="header-menu-item-wrapper">
-            <div className="header-logo-menu-wrapper">
+          <div className="align-items-center d-flex header-menu-item-wrapper justify-content-between">
+            <div className="header-logo-menu-wrapper d-flex align-items-center">
               <div className="header-logo-wrapper">
                 <Link to="/" className="header-logo-link">
                   <img src={logo} alt="The Movie Database (TMDb)" />
                 </Link>
               </div>
+              <div className="header-logo-menu">
+                <ul className="navbar-nav list-inline mb-0">
+                  <li className="nav-item list-inline-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/"
+                      exact
+                      activeClassName="active"
+                    >
+                      Home
+                    </NavLink>
+                  </li>
+                  <li className="nav-item list-inline-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/tv-shows"
+                      exact
+                      activeClassName="active"
+                    >
+                      TV Shows
+                    </NavLink>
+                  </li>
+                  <li className="nav-item list-inline-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/movies"
+                      exact
+                      activeClassName="active"
+                    >
+                      Movies
+                    </NavLink>
+                  </li>
+                  <li className="nav-item list-inline-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/popular"
+                      exact
+                      activeClassName="active"
+                    >
+                      Popular
+                    </NavLink>
+                  </li>
+                  <li className="nav-item list-inline-item">
+                    <NavLink
+                      className="nav-link"
+                      to="/my-list"
+                      exact
+                      activeClassName="active"
+                    >
+                      My List
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="header-right-menu-wrapper">
+              <ul className="navbar-nav list-inline mb-0">
+                <li className="nav-item list-inline-item">
+                  <button type="button" className="btn">
+                    <span className="material-icons-outlined">search</span>
+                  </button>
+                </li>
+                <li className="nav-item list-inline-item">
+                  <button type="button" className="btn">
+                    <span className="material-icons-outlined">translate</span>
+                  </button>
+                </li>
+                <li className="nav-item list-inline-item">
+                  <button type="button" className="btn">
+                    <span className="material-icons">notifications</span>
+                  </button>
+                </li>
+                <li className="nav-item list-inline-item">
+                  <button type="button" className="btn">
+                    <span className="material-icons">account_circle</span>
+                  </button>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
