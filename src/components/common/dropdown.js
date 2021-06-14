4bca0ee6e4efe2dc86102a81fe3098a19c998523
arunbisht <aabisht@gmail.com>
2021-06-14 16:55:47 +0530

    Implimented Dropdown.

--- src/components/common/dropdown.js
+++ src/components/common/dropdown.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 function DropDown(props) {
@@ -6,12 +6,51 @@ function DropDown(props) {
   if (props.dropdownPosition) {
     wrapperClass += " " + props.dropdownPosition;
   }
+
+  useEffect(() => {
+    document.body.addEventListener("click", handleCloseDropdownonBodyClick);
+    return () =>
+      window.removeEventListener("click", handleCloseDropdownonBodyClick);
+  }, []);
+
+  let handleDropDownToggle = (event) => {
+    event.stopPropagation();
+    let _this = event.currentTarget;
+
+    for (let sibling of _this.parentNode.children) {
+      sibling.classList.contains("show")
+        ? sibling.classList.remove("show")
+        : sibling.classList.add("show");
+    }
+  };
+
+  let handleDropdownListClick = (event) => {
+    event.stopPropagation();
+  };
+
+  let handleCloseDropdownonBodyClick = () => {
+    for (let element of document.querySelectorAll(".dropdown-btn.show")) {
+      element.classList.remove("show");
+    }
+    for (let element of document.querySelectorAll(
+      ".dropdown-list-wrapper.show"
+    )) {
+      element.classList.remove("show");
+    }
+  };
+
   return (
     <div className="dropdown-wrapper">
-      <button type="button" className="btn dropdown-btn">
+      <button
+        type="button"
+        className="btn dropdown-btn"
+        onClick={handleDropDownToggle}
+      >
         {props.dropdownText}
       </button>
-      <div className={wrapperClass}>{props.dropdownList}</div>
+      <div className={wrapperClass} onClick={handleDropdownListClick}>
+        {props.dropdownList}
+      </div>
     </div>
   );
 }
